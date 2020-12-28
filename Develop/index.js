5028e472b6d14d9593ba601e9dfa6df7ee54de97
buildgal <gjaquelin7@outlook.com>
2020-12-27 21:46:10 -0600

    adding the license options

--- Develop/index.js
+++ Develop/index.js
@@ -37,6 +37,15 @@ const userQuestion = () => {
             name: 'test',
             message:'What are the test instructions?'
         }
+
+        ,
+
+        {
+            type: 'list',
+            name: 'license',
+            message:'What license are you using?',
+            choices: ["MIT","Apache-2.0","GPL-3.0","BSD-2-Clause","BSD-3-Clause","BSD-4-Clause"]
+        }
     ])
 }
     

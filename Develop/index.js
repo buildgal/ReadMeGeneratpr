e4e520531a354b85c8dd56991cbc7383b7c1dca0
buildgal <gjaquelin7@outlook.com>
2020-12-28 22:39:56 -0600

    reformatting code to match the functions outlined

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,8 @@
 const inquirer = require ("inquirer");
 const fs = require('fs');
+const util = require ("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const userQuestion = () => {
     return inquirer.prompt([
@@ -59,38 +62,59 @@ const userQuestion = () => {
         }
 
     ])
-    .then((data) => {
-        writeToFile("readMe.json",data);
-        //this is where the code is breaking
-    })
-    
-    
-}
-    
+};
 
-//WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-const questions = [
-];
+/*
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.description}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+    */
 
 
 
 // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.log(err) : console.log('Success!')
-  );
+    fs.writeFile(filename, data, err =>{
+        if (err) {
+            throw err;
+        }else{
+          console.log("Success!")
+        }
+    });
 }
+
+
 
 // function to initialize program
 function init() {
+    userQuestion()
+    .then((answers) => writeFileAsync('./READNME.md', generateREADME(answers)))
+    .catch((err) => console.error(err));
 
 }
 
 // function call to initialize program
 init();
 
-//this is what kicks off the program to write out in the command line
-userQuestion()
-  //.then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-  .then(() => console.log('Successfully wrote to in markup language'))
-  .catch((err) => console.error(err));
+
+
